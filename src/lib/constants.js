c3238bb282384cbdb5521b96a396d13442fdd7a4
Chris Sarmiento <chrissarmiento@hotmail.com>
2021-03-03 08:54:29 +0000

    Remove seconds from TIME_FORMAT

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 const C = {
   DATE_FORMAT: 'DD-MMM-YY',
-  TIME_FORMAT: 'HH:mm:ss',
+  TIME_FORMAT: 'HH:mm',
   DATETIME_FORMAT: 'DD-MMM-YY / HH:mm:ss',
   TIMEFRAMES: ["m1", "m5", "m15", "m30", "H1", "H2", "H3", "H4", "H6", "H8", "D1", "W1", "M1"],
   historicalID: ({ pair, timeframe }) => `${pair}-${timeframe}`
